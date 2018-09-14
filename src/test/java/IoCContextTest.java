c9c554e637293385edab4844026958434bb25e54
Qingqing Dou <qqdou@thoughtworks.com>
2018-09-14 15:34:53 +0800

    format code
    Signed-off-by: Qingqing Dou <qqdou@thoughtworks.com>

--- src/test/java/IoCContextTest.java
+++ src/test/java/IoCContextTest.java
@@ -15,31 +15,31 @@ class IoCContextTest {
     @Test
     void should_throw_exception_if_null() {
         IoCContextImpl<MyBean> context = new IoCContextImpl<>();
-        assertThrows(IllegalArgumentException.class,()-> context.registerBean(null),"beanClazz is mandatory");
+        assertThrows(IllegalArgumentException.class,() -> context.registerBean(null),"beanClazz is mandatory");
     }
 
     @Test
     void should_throw_exception_if_not_can_be_instance() {
         IoCContextImpl<FunctionalInterface> context = new IoCContextImpl<>();
-        assertThrows(IllegalArgumentException.class,()-> context.registerBean(FunctionalInterface.class),MyInterface.class.getSimpleName()+" is abstract");
+        assertThrows(IllegalArgumentException.class,() -> context.registerBean(FunctionalInterface.class),MyInterface.class.getSimpleName()+" is abstract");
     }
 
     @Test
-    void should_throw_exception_if_not_have_default_constroctor() {
+    void should_throw_exception_if_not_have_default_constructor() {
         IoCContextImpl<WithNoDefaultConstructor> context = new IoCContextImpl<>();
-        assertThrows(IllegalArgumentException.class,()-> context.registerBean(WithNoDefaultConstructor.class),WithNoDefaultConstructor.class.getSimpleName()+" has no default constructor");
+        assertThrows(IllegalArgumentException.class,() -> context.registerBean(WithNoDefaultConstructor.class),WithNoDefaultConstructor.class.getSimpleName()+" has no default constructor");
     }
 
     @Test
     void should_throw_exception_if_resolveClazz_is_null() {
         IoCContextImpl<MyBean> context = new IoCContextImpl<>();
-        assertThrows(IllegalArgumentException.class,()-> context.getBean(null));
+        assertThrows(IllegalArgumentException.class,() -> context.getBean(null));
     }
 
     @Test
     void should_throw_exception_if_not_register() {
         IoCContextImpl<MyBean> context = new IoCContextImpl<>();
-        assertThrows(IllegalStateException.class,()-> context.getBean(MyBean.class));
+        assertThrows(IllegalStateException.class,() -> context.getBean(MyBean.class));
     }
 
     @Test
@@ -47,6 +47,6 @@ class IoCContextTest {
         IoCContextImpl<MyBean> context = new IoCContextImpl<>();
         context.registerBean(MyBean.class);
         context.getBean(MyBean.class);
-        assertThrows(IllegalStateException.class,()-> context.registerBean(MyBean.class));
+        assertThrows(IllegalStateException.class,() -> context.registerBean(MyBean.class));
     }
 }
