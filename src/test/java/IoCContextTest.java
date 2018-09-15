d9935da53d3fc9257305efa45fc37d71946da262
Qingqing Dou <qqdou@thoughtworks.com>
2018-09-15 13:22:00 +0800

    add class and interface test
    Signed-off-by: Qingqing Dou <qqdou@thoughtworks.com>

--- src/test/java/IoCContextTest.java
+++ src/test/java/IoCContextTest.java
@@ -7,37 +7,37 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class IoCContextTest {
     @Test
     void should_throw_exception_if_null() {
-        IoCContextImpl<MyBean> context = new IoCContextImpl<>();
+        IoCContextImpl context = new IoCContextImpl();
         assertThrows(IllegalArgumentException.class,() -> context.registerBean(null),"beanClazz is mandatory");
     }
 
     @Test
     void should_throw_exception_if_not_can_be_instance() {
-        IoCContextImpl<FunctionalInterface> context = new IoCContextImpl<>();
+        IoCContextImpl context = new IoCContextImpl();
         assertThrows(IllegalArgumentException.class,() -> context.registerBean(FunctionalInterface.class), MyInterface.class.getSimpleName()+" is abstract");
     }
 
     @Test
     void should_throw_exception_if_not_have_default_constructor() {
-        IoCContextImpl<WithNoDefaultConstructor> context = new IoCContextImpl<>();
+        IoCContextImpl context = new IoCContextImpl();
         assertThrows(IllegalArgumentException.class,() -> context.registerBean(WithNoDefaultConstructor.class),WithNoDefaultConstructor.class.getSimpleName()+" has no default constructor");
     }
 
     @Test
     void should_throw_exception_if_resolveClazz_is_null() {
-        IoCContextImpl<MyBean> context = new IoCContextImpl<>();
+        IoCContextImpl context = new IoCContextImpl();
         assertThrows(IllegalArgumentException.class,() -> context.getBean(null));
     }
 
     @Test
     void should_throw_exception_if_not_register() {
-        IoCContextImpl<MyBean> context = new IoCContextImpl<>();
+        IoCContextImpl context = new IoCContextImpl();
         assertThrows(IllegalStateException.class,() -> context.getBean(MyBean.class));
     }
 
     @Test
     void should_not_register_after_getBean() throws InstantiationException, IllegalAccessException {
-        IoCContextImpl<MyBean> context = new IoCContextImpl<>();
+        IoCContextImpl context = new IoCContextImpl();
         context.registerBean(MyBean.class);
         context.getBean(MyBean.class);
         assertThrows(IllegalStateException.class,() -> context.registerBean(MyBean.class));
@@ -45,12 +45,12 @@ class IoCContextTest {
 
     @Test
     void should_throw_exception_if_getBean() {
-        IoCContextImpl<MyBeanWithError> context = new IoCContextImpl<>();
+        IoCContextImpl context = new IoCContextImpl();
         assertThrows(ArithmeticException.class, ()->context.registerBean(MyBeanWithError.class));
     }
     @Test
     void should_register_bean_correct() throws InstantiationException, IllegalAccessException {
-        IoCContextImpl<MyBean> context = new IoCContextImpl<>();
+        IoCContextImpl context = new IoCContextImpl();
         context.registerBean(MyBean.class);
         MyBean myBean = context.getBean(MyBean.class);
         assertEquals(myBean.getClass(), MyBean.class);
@@ -58,7 +58,7 @@ class IoCContextTest {
 
     @Test
     void should_register_2_get_1() throws InstantiationException, IllegalAccessException {
-        IoCContextImpl<?> context = new IoCContextImpl<>();
+        IoCContextImpl context = new IoCContextImpl();
         context.registerBean(MyBean.class);
         context.registerBean(Student.class);
         MyBean myBean = context.getBean(MyBean.class);
@@ -66,7 +66,7 @@ class IoCContextTest {
     }
     @Test
     void should_register_2_get_2() throws InstantiationException, IllegalAccessException {
-        IoCContextImpl<?> context = new IoCContextImpl<>();
+        IoCContextImpl context = new IoCContextImpl();
         context.registerBean(MyBean.class);
         context.registerBean(Student.class);
         MyBean myBean = context.getBean(MyBean.class);
@@ -77,7 +77,7 @@ class IoCContextTest {
 
      @Test
     void should_register_2_get_anyone() throws InstantiationException, IllegalAccessException {
-        IoCContextImpl<?> context = new IoCContextImpl<>();
+        IoCContextImpl context = new IoCContextImpl();
         context.registerBean(MyBean.class);
         context.registerBean(Student.class);
         Student student = context.getBean(Student.class);
